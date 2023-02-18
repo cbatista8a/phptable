52ae63f5a7c802c3911a6f33930b677d47901cb4
Carlos Batista <cbatista8a@gmail.com>
2023-02-18 17:58:05 +0100

    add a dummy test for Table class

---
+++ tests/TableTest.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace Cbatista8a\PhpTable\Test;
+
+use Cbatista8a\PhpTable\Domain\Table;
+use PHPUnit\Framework\TestCase;
+
+class TableTest extends TestCase
+{
+
+    public function testRender()
+    {
+        //Todo write test assertions
+        self::markTestSkipped();
+    }
+
+    public function testAddRow()
+    {
+        //Todo write test assertions
+        self::markTestSkipped();
+    }
+}
